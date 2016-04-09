c6e2cda5c43463a271a03e3f772370fff2833eba
Ashok Ramaraj - aramar1 <aramar1@L-SNV5EDJDF8-M.local>
2016-04-09 01:48:50 -0700

    adding new CXF sample for adalwin

---
+++ src/main/java/com/adalwin/cxfrestservice/internal/CxfRestServiceImpl.java
@@ -0,0 +1,24 @@
+package com.adalwin.cxfrestservice.internal;
+
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.adalwin.cxfrestservice.CxfRestService;
+import com.adalwin.cxfrestservice.dao.EmployeeDao;
+
+public class CxfRestServiceImpl implements CxfRestService 
+{
+	@Autowired
+	private EmployeeDao employeeDao; 
+
+	@Override
+	public Response getEmployeeDetail(String employeeId) 
+	{
+		if(employeeId == null)
+		{
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}		
+		return Response.ok(employeeDao.getEmployeeDetails(employeeId)).build();
+	}
+}
